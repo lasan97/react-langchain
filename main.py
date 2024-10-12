ebefac9232cd70f8de3299b8aa5b76a8b7c529e6
Martin <martin@niceday.io>
2024-10-12 18:28:25 +0900

    simple agent

--- main.py
+++ main.py
@@ -1,11 +1,84 @@
+from typing import Union, List
+
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description, Tool
+from langchain_community.chat_models import ChatOpenAI
+
 load_dotenv()
 
 
-def get_text_length(text:str) -> int:
+@tool
+def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
+
     return len(text)
 
-if __name__ == '__main__':
+
+def find_tool_by_name(tools: List[Tool], tools_name:str):
+    for tool in tools:
+        if tool.name == tools_name:
+            return tool
+    raise ValueError(f"Tool with name {tools_name} not found")
+
+
+if __name__ == "__main__":
     print("Hello ReAct LangChain")
-    print(get_text_length(text="Dog"))
+
+    tools = [get_text_length]
+
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools])
+    )
+
+    llm = ChatOpenAI(
+        temperature=0,
+        model_kwargs={"stop":["\nObservation", "Observation"]}
+    )
+
+    agent = {"input": lambda x:x["input"]} |prompt | llm | ReActSingleInputOutputParser()
+
+    # res = agent.invoke({"input": "What is the text length of 'DOG' in characters?" })
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {"input": "What is the text length in characters of the 'DOG' ?"}
+    )
+
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation}")
+        
